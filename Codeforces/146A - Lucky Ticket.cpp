81b6b31d8de7762b960fe8a49cb5800fecdf931d
tyam-vm <117309997+tyam-vm@users.noreply.github.com>
2024-08-07 17:59:02 +0530

    Update and rename file to 146A - Lucky Ticket.cpp

---
+++ Codeforces/146A - Lucky Ticket.cpp
@@ -0,0 +1,29 @@
+// problem15
+#include <iostream>
+void solve();
+using namespace std;
+int p146A()
+{
+    int n(1);
+    // cin >> n;
+
+    while (n--)
+    {
+        solve();
+    }
+    return 0;
+}
+void solve()
+{
+    int len, first(0), last(0);
+    string input;
+    cin >> len >> input;
+    for (int i(0); i < len; i++)
+    {
+        if (input[i] != '4' and input[i] != '7'){ cout << "NO"; return; }
+        if (i < len / 2) { first += int(input[i]); }
+        else { last += int(input[i]); }
+    }
+    if (first == last) { cout << "YES"; }
+    else cout << "NO";
+}
